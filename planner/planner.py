8ccbc01f4c5908d353f5b5b3e91001fb41350682
Carey Underwood <cwillu@cwillu.com>
2016-06-14 21:06:18 -0600

    asdf

--- planner/planner.py
+++ planner/planner.py
@@ -11,9 +11,10 @@ def path():
   data = point.make_mmapped_data(d, f=open('mmap', 'rb+'))
 
   for p in xrange(d.x * d.y):
-    if data[p] != 0xffffffff:
-      # data[p] = 0xffffffff
-      data[p] = 0x0
+    data[p] = 0xffffffff
+    # if data[p] != 0xffffffff:
+    #   # data[p] = 0xffffffff
+    #   data[p] = 0x0
 
   def get_point(p):
     p //= 1
@@ -25,10 +26,11 @@ def path():
     p //= 1
     if p % d != p:
       raise ValueError("Value out of range: {} {}".format(p, d))
-    data[p.x + p.y * d.x] = v
+    if v != 0 or data[p.x + p.y * d.x] != 0x00ccccff:
+      data[p.x + p.y * d.x] = v
 
   current = P(500.0, 500.0)
-  radius = 50.0
+  radius = 190.0
   radius_sq = radius ** 2
   max_turn = 1.0/16
 
@@ -42,7 +44,23 @@ def path():
   feed_step = 1
 
   working = P(0.0, 0.0)
+
+  cut_points = []
+  for rise in xrange(0, int(radius+1)):
+    bound = int(round(math.sqrt(radius_sq - rise**2)))
+    for stroke in [-bound, -bound + 1, bound-1, bound]: #xrange(-bound, bound + 1):
+      cut_points.append(P(rise, stroke))
+      cut_points.append(P(-rise, stroke))
+      cut_points.append(P(stroke, rise))
+      cut_points.append(P(stroke, -rise))
+
+  cut_points = list(set(cut_points))
+  cut_points.sort()
+
+
   scan_point = P(0.0,0.0)
+  old_current = P(0,0)
+  old_direction = P(0,0)
   while True:
     scan_point |= direction
     scan_point *= radius + 1
@@ -58,12 +76,11 @@ def path():
         working |= current
         working += scan_point
         material = get_point(working)
-        print '<',
+        # print '<',
         # set_point(current + scan_point, 0)
         if material != 0xffffffff:
           break
     else:
-      old_direction = direction / 1
       set_point(working, 0x0000ff00)
       scan_point >>= yaw_step
       working |= current
@@ -72,7 +89,7 @@ def path():
       for step in xrange(10):
         if material == 0xffffffff:
           break
-        print '>',
+        # print '>',
         direction >>= yaw_step
         scan_point >>= yaw_step
         working |= current
@@ -84,29 +101,34 @@ def path():
 
     working |= current + direction
     if working//1 != current//1:
+      for r in xrange(int(radius-5), int(radius+1)):
+        working |= old_current
+        working += old_direction*r
+        set_point(working, 0)
       working |= current
-      for y in xrange(int(radius-5), int(radius+1)):
-        bound = int(round(math.sqrt(radius_sq - y**2)))
-        for x in xrange(-bound, bound + 1):
-          color = 0# if x and y else 0x000000ff
-          working |= current
-          working += x, y
-          set_point(working, color)
-          working |= current
-          working += x, -y
-          set_point(working, color)
-        for r in xrange(0, int(radius+1)):
-          working |= current
-          working += direction*r
-          set_point(working, 0x00ffff00)
+      for cut_point in cut_points:
+        working |= current
+        working += cut_point
+        set_point(working, 0)
+
+
+      for r in range(int(radius-5), int(radius+1)):
+        working |= current
+        working += direction*r
+        set_point(working, 0x00ffff00)
+      working |= current
+      set_point(working, 0x00ccccff)
+
+      old_current |= current
+      old_direction |= direction
 
 
 
 
-    print
+    # print
     current = current + direction
     # current += direction c
-    print '{} {} {:.2f} {} {}'.format(math.hypot(direction.x, direction.y), direction, math.degrees(math.acos(direction.x)), current, current)
+    # print '{} {} {:.2f} {} {}'.format(math.hypot(direction.x, direction.y), direction, math.degrees(math.acos(direction.x)), current, current)
     # import time
     # time.sleep(0.)
 
